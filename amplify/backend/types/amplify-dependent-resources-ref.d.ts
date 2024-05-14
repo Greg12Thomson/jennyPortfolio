733e3afd169dc7bd98f175f18b28457df7853658
GregisGinger <Greg12thomson@Gmail.com>
2024-05-14 16:33:33 +0100

    Add amplify storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "jennyportfolio23a92a71": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "portfolio": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}

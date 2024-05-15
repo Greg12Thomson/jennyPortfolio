57da64ba91848ae63bc769905c23586a61384532
GregisGinger <Greg12thomson@Gmail.com>
2024-05-15 16:34:13 +0100

    Fix see more button placement

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 import { Col, Row, Button } from 'react-bootstrap';
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 
-// amplify config
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from '../amplifyconfiguration.json';
-
-Amplify.configure(amplifyconfig);
-
 
 const porfolioImages = Array(47).fill(0).map((_, i) => (
     <StorageImage alt={`portfolio-image-${i}`} path={`public/${i}.jpg`}  className="portfolio-photo" key={i} />
@@ -32,9 +26,13 @@ function Photo() {
             <Col md={12}>
                 <div className="photo">
                     <h2 onClick={() => setShow(!show)}>Photo</h2>
-                    <div className={show ? "photo-content" : "hide"}>
-                        {porfolioImages.slice(10 * (page - 1), 10 * page)}
-                        <Button className="porfolio-button" onClick={onClick}>See more</Button>
+                    <div className={show ? "photo-container" : "hide"}>
+                        <div className="photo-content">
+                            {porfolioImages.slice(10 * (page - 1), 10 * page)}
+                        </div>
+                        <Button className="porfolio-button" onClick={onClick}>
+                            See more
+                        </Button>
                     </div>
                 </div>
             </Col>
